e9c9071026c38ee46f8ffd56700758dbc89d5b3f
drinkurtishi <drin.kurtishi@hotelkeyapp.com>
2024-09-21 10:06:12 +0200

    Enviroment setup

---
+++ src/main/java/com/drin/httpserver/HttpServer.java
@@ -0,0 +1,15 @@
+package com.drin.httpserver;
+
+public class HttpServer {
+
+    public static void main(String[] args) {
+        System.out.println("Server Starting");
+        //Steps:
+        //Read Configuration Files
+        //Open a socket to listen to a Port
+        //Read request messages
+        //Open and read files from the FileSystem
+        //Write Response Messages
+    }
+
+}
