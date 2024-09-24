fb33aedab654e3699c97790fa5e386067963e352
drinkurtishi <drin.kurtishi@hotelkeyapp.com>
2024-09-24 21:48:29 +0200

    Dumbest http server known to man  done (only responds with a hardcoded html page)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 A simple HTTP server to practice Computer Networking, Sockets, Multithreading etc..
+
+features (ongoing):
+    - Server only allows one connection at a time, one browser connected to the server and were going to serve a page to that browser
+    - its really dumb, only sends a hardcoded page to the browser, it ignores any request from the browser.
+    - I will handle multithreading and http in the future.
--- src/main/java/com/drin/httpserver/HttpServer.java
+++ src/main/java/com/drin/httpserver/HttpServer.java
@@ -1,5 +1,11 @@
 package com.drin.httpserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import com.drin.httpserver.config.Configuration;
 
 import com.drin.httpserver.config.ConfigurationManager;
@@ -13,6 +19,36 @@ public class HttpServer {
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
         System.out.println("Using port: " + conf.getPort());
         System.out.println("Using webroot: " + conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            
+            //writing
+            String html = "<html><head><title>Simple java http server</title></head><body><h1>This page was served using my own server</h1></body></html>";
+
+            final String CRLF = "\n\r"; //13, 10
+
+            String response = 
+                              "HTTP/1.1 200 OK" + CRLF +
+                              "Content-length: " + html.getBytes().length + CRLF + 
+                              CRLF +
+                              html +
+                              CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            serverSocket.close();
+            socket.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
